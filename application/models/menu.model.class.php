3bb9a4fbec42b72432e40281060a3b4250c67a3a
Rain <rainelemental@gmail.com>
2011-02-11 19:40:38 +0100

    create the menu with controllers list

--- application/models/menu.model.class.php
+++ application/models/menu.model.class.php
@@ -3,10 +3,11 @@
 	class Menu_Model extends Model{
 
 		function load_menu( $selected = null){
-			//$this->menu = file_list( CONTROLLERS_DIR, "class.php" );
-			$menu = array( 'content', 'test' );
-			foreach( $menu as $voice )
+			$menu = file_list( CONTROLLERS_DIR, "class.php" );
+			foreach( $menu as $voice ){
+				$voice = array_shift( @explode(".",$voice));
 				$menu_list[] = array('name'=>$voice, 'link'=> 'index.php/' . $voice . '/', 'selected' => $selected==$voice );
+			}
 			return $menu_list;
 		}
 
