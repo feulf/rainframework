e2cf37942171589a86db8ee662b1ed50522c558f
Rain <rainelemental@gmail.com>
2011-02-11 20:28:38 +0100

    clear cache

--- application/models/menu.model.class.php
+++ application/models/menu.model.class.php
@@ -3,7 +3,7 @@
 	class Menu_Model extends Model{
 
 		function load_menu( $selected = null){
-			$menu = file_list( CONTROLLERS_DIR, "class.php" );
+			$menu = file_list( CONTROLLERS_DIR, "php" );
 			foreach( $menu as $voice ){
 				$voice = array_shift( @explode(".",$voice));
 				$menu_list[] = array('name'=>$voice, 'link'=> 'index.php/' . $voice . '/', 'selected' => $selected==$voice );
