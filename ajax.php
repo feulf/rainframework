54fa648831e7f6729bec550eed48572b1b8efd70
Rain <rainelemental@gmail.com>
2011-03-21 18:00:51 +0100

    added $application to select the base application directory

--- ajax.php
+++ ajax.php
@@ -4,19 +4,29 @@
 	session_start();
 
 
-        
+
 
 	#--------------------------------
+	# Base application directory
+	#--------------------------------
+        $application = 'application';
+
+
+
+        #--------------------------------
 	# Load the class
 	#--------------------------------
-	require_once "application/config/constants.php";
+	require_once "$application/config/constants.php";
 	require_once "library/Loader.php";
-        require_once "application/Bootstrap.Loader.php";
+        require_once "$application/Bootstrap.Loader.php";
 
 
 
+	#--------------------------------
+	# Load the bootstrap
+	#--------------------------------
         $loader = new Bootstrap_Loader;
-        $loader->init_ajax();
+        $loader->init_ajax();   // ajax functions
 
 
 
--- index.php
+++ index.php
@@ -7,14 +7,24 @@
 
 
 	#--------------------------------
+	# Base application directory
+	#--------------------------------
+        $application = 'application';
+
+
+
+        #--------------------------------
 	# Load the class
 	#--------------------------------
-	require_once "application/config/constants.php";
+	require_once "$application/config/constants.php";
 	require_once "library/Loader.php";
-        require_once "application/Bootstrap.Loader.php";
+        require_once "$application/Bootstrap.Loader.php";
 
 
-        
+
+	#--------------------------------
+	# Load the bootstrap
+	#--------------------------------
         $loader = new Bootstrap_Loader;
         $loader->init();
 
