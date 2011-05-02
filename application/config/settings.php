f4ac3ef4ab1c5045b8bd3424c49c18232e97e254
Rain <rainelemental@gmail.com>
2011-05-02 12:52:51 +0200

    removed page not found from settings

--- application/config/settings.php
+++ application/config/settings.php
@@ -6,7 +6,6 @@
 	global $settings;
 
 	$settings['timezone'] 			= "America/New_York";    // server timezone
-	$settings['page_not_found'] 		= "not_found";      // page not found
         $settings['url'] = str_replace( basename( $_SERVER['PHP_SELF'] ), '', 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']) . "/" );
 	define( "URL", $settings['url'] ); // base url
 
