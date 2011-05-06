9bbe8704f88428dbe6b39e5b4cde27aad3c7ac8d
Rain <rainelemental@gmail.com>
2011-05-06 22:31:04 +0200

    cleaning

--- application/controllers/content/Content.php
+++ application/controllers/content/Content.php
@@ -4,11 +4,6 @@
 
 		function index(){
 
-                    $db = db::get_instance();
-$array_team = array('lastname' => "eeeeeeeeee");
-$db->update("rain_user", $array_team, "user_id = 1");
-                    
-                    
 			//love easy
 			$this->load_model("content","content_obj");
 
