f9e032f995b19d536b28523dbacbf8fea526d1ae
Rain <rainelemental@gmail.com>
2011-05-07 11:52:01 +0200

    index moved to framework root

---
+++ ajax.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+	#--------------------------------
+	# Base application directory
+	#--------------------------------
+        $application = "application";
+        $website     = "website";
+
+
+
+        #--------------------------------
+	# Load the class
+	#--------------------------------
+	require_once "$website/config/directory.php";
+
+
+
+
+	#--------------------------------
+	# Load the bootstrap
+	#--------------------------------
+        require_once "$application/ajax.bootstrap.php";
