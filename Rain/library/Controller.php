f2d29a09b5ea1fff148197f452903fc00ea71dcb
Rain <rainelemental@gmail.com>
2011-05-23 01:08:43 +0200

    controller fix

--- Rain/library/Controller.php
+++ Rain/library/Controller.php
@@ -18,48 +18,6 @@ class Controller{
         static protected $controllers_dir = CONTROLLERS_DIR;
         static protected $controller_loaded = array();
 
-
-
-        /**
-         * load a controller and return the html
-         * 
-         * @param string $controller, controller name
-         * @param string $object_name, object name (eg. if you use $this->load_controller("user","user_obj"); you can access the controller with $this->user_obj->...
-         * @param string $controller_extension, extension of the controller filename
-         * @param string $controller_class_name, extension of the class name
-         * @return bool 
-         */
-        function load_controller( $controller, $object_name = null, $controller_extension = CONTROLLER_EXTENSION, $controller_class_name = CONTROLLER_CLASS_NAME ){
-
-                // if you don't se the object name, the object name is the controller name
-		if(!$object_name)
-			$object_name = $controller;
-
-                // transform the controller string to capitalized. e.g. user => User, news_list => News_List
-                $controller = implode( "_", array_map( "ucfirst", array_map( "strtolower", explode( "_", $controller ) ) ) );
-
-		// include the controller file
-		if( file_exists( $controller_file = self::$controllers_dir . "$controller/$controller." . $controller_extension ) )
-			require_once $controller_file;
-		else{
-                        // if the controller is not found it throw a WARNING and return false
-                        trigger_error( "CONTROLLER: FILE <b>{$controller_file}</b> NOT FOUND ", E_USER_WARNING );
-                        return false;
-                }
-
-                // compose the class name
-		$class = $controller . $controller_class_name;
-
-                // check if the class exists
-		if( class_exists($class) )
-			$this->$object_name = new $class( $this );
-		else
-			return trigger_error( "CONTROLLER: CLASS <b>{$controller}</b> NOT FOUND ", E_USER_WARNING );
-
-                self::$controller_loaded[] = array( 'controller'=>$controller );
-        }
-
-
 	/**
 	 * load a controller and return the html
 	 *
@@ -69,7 +27,6 @@ class Controller{
                 // get the loader
                 $loader = Loader::get_instance();
                 $loader->load_controller( $controller, $action, $params, $load_area );
-
 
         }
 
