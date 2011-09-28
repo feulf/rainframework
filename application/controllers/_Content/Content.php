a451cf02472e4ab56dafedbb9da57cd9813f5a40
rainphp <rainelemental@gmail.com>
2011-09-27 23:55:23 -0400

    Temp rename

---
+++ application/controllers/_Content/Content.php
@@ -0,0 +1,21 @@
+<?php
+
+	class Content_Controller extends Controller{
+            
+		function index(){
+
+			//love easy
+			$this->load_model("content","content_obj");
+
+			$content_row = $this->content_obj->get();
+
+			// Load view
+			$tpl = new View;
+			$tpl->assign( $content_row );
+			$tpl->draw('content/content');
+
+		}
+
+	}
+	
+?>
---
+++ application/controllers/_Form/Form.Ajax.php
@@ -0,0 +1,15 @@
+<?php
+
+	class Form_Ajax_Controller extends Controller{
+
+		function save(){
+
+			// set the output of the ajax mode
+			$this->ajax_mode( true, true );
+			echo $name = get_post('name');
+
+		}
+
+	}
+	
+?>
