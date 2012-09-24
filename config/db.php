03cd585dcad9c8f60c191365949d1ffad84e7bc6
faryshta <angeldelcaos@gmail.com>
2012-09-24 15:29:10 -0500

    change on the db config file so it can be more esay to read for humans

--- config/db.php
+++ config/db.php
@@ -1,21 +1,23 @@
-<?php
+<?
+$db=array(
+	// development database (default)
+	'dev'=>array(
+		'driver'=>'mysql',
+		'hostname'=>'localhost',
+		'username'=>'root',
+		'password'=>'root',
+		'database'=>'rainframework2'
+	),
+	//production database (live website)
+	'prod'=>array(
+		'driver'=>'',
+		'hostname'=>'',
+		'username'=>'',
+		'password'=>'',
+		'database'=>''
+	)
+);
 
-
-    // development database (default)
-    $db['dev']['driver']    = 'mysql';
-    $db['dev']['hostname']  = 'localhost';
-    $db['dev']['username']  = 'root';
-    $db['dev']['password']  = 'root';
-    $db['dev']['database']  = 'rainframework2';
-
-    // production database (live website)
-    $db['prod']['driver']    = '';
-    $db['prod']['hostname']  = '';
-    $db['prod']['username']  = '';
-    $db['prod']['password']  = '';
-    $db['prod']['database']  = '';
-
-	if( !defined("DB_PREFIX" ) )
-		define( "DB_PREFIX", "RAIN_" );
-
+if( !defined("DB_PREFIX" ) )
+	define( "DB_PREFIX", "RAIN_" );
 // -- end
